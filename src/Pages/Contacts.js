bc22fe5fdad0fb631622a6798d4afe523dd629d9
AntonMihailov <you@example.com>
2020-05-10 10:04:31 +0300

    feat: add contacts cards

--- src/Pages/Contacts.js
+++ src/Pages/Contacts.js
@@ -1,11 +1,54 @@
 import React, { Component } from 'react';
-import { Form, Button, Container} from 'react-bootstrap';
+import { Card, Container, Row, Col } from 'react-bootstrap';
 
 export default class Contacts extends Component {
     render() {
         return(
             <>
             <h2>Контакты</h2><hr></hr>
+            <h3>г. Минск, ул. Восточная 133, каб.904</h3>
+            <Container>
+            <Row>
+            <Card style={{ width: '18rem' }}>
+                <Card.Body>
+                    <Card.Subtitle className="mb-2 text-muted">Станция метро</Card.Subtitle>
+                    <Card.Text>
+                        Якуба Коласа
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+
+            <Card style={{ width: '18rem' }}>
+                <Card.Body>
+                    <Card.Subtitle className="mb-2 text-muted">Режим работы</Card.Subtitle>
+                    <Card.Text>
+                        09:00 - 18:00
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+            </Row>
+
+            <Row>
+            <Card style={{ width: '18rem' }}>
+                <Card.Body>
+                    <Card.Subtitle className="mb-2 text-muted">E-mail</Card.Subtitle>
+                    <Card.Text>
+                        pochta@example.com
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+
+            <Card style={{ width: '18rem' }}>
+                <Card.Body>
+                    <Card.Subtitle className="mb-2 text-muted">Телефон</Card.Subtitle>
+                    <Card.Text>
+                        +375 (29) 111-11-11
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+            </Row>
+            </Container>
+            
             </>
         )
     }
